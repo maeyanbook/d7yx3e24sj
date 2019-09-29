8643ce2adf5cba92acaffcfdb7f73394d4c626fe
maeyan <y.maeyan@gmail.com>
2019-09-29 14:03:27 +0900

    一部、空フォルダの扱いを変更(もともと用意しているからプログラムで生成）

--- HajimetenoWindowsBatch/batch/06/sample54.bat
+++ HajimetenoWindowsBatch/batch/06/sample54.bat
@@ -1,4 +1,7 @@
 @echo off
 
+rem フォルダがない時作る
+if not exist .\data\sample54\aa\empty ( mkdir .\data\sample54\aa\empty )
+
 rem aaフォルダをbbの中に保存したい(NG))
 echo.d| xcopy ".\data\sample54\aa" ".\data\sample54\bb\aa" /y
--- HajimetenoWindowsBatch/batch/06/sample55.bat
+++ HajimetenoWindowsBatch/batch/06/sample55.bat
@@ -1,4 +1,7 @@
 @echo off
 
+rem フォルダがない時作る
+if not exist .\data\sample55\aa\empty ( mkdir .\data\sample55\aa\empty )
+
 rem aaフォルダをbbの中に保存したい
 echo.d| xcopy ".\data\sample55\aa" ".\data\sample55\bb\aa" /y /e
